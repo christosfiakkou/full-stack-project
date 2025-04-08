b79244d4c00e8ac63a1302293bdaa638fe02f850
christosfiakkou <fiakkouchristos@gmail.com>
2025-04-08 12:53:38 +0300

    Add DATABASE_URL for Render deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,6 +1,9 @@
 import os
-from decouple import config
 from pathlib import Path
+
+from decouple import config
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,21 +73,23 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": config("POSTGRES_DB"),
+#        "USER": config("POSTGRES_USER"),
+#        "PASSWORD": config("POSTGRES_PASSWORD"),
+#        "HOST": config("POSTGRES_HOST"),
+#        "PORT": config("POSTGRES_PORT"),
+#    }
+# }
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST"),
-        "PORT": config("POSTGRES_PORT"),
-    }
+    "default": dj_database_url.config(
+        default=config("DATABASE_URL")  # Uses env var from Render
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
